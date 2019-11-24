e481c97fe0ff328a3140608c8e47d7a197525ae8
oussama benali <oussama.benali.3@esprit.tn>
2019-11-24 21:05:13 +0100

    test

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new ForumBundle\ForumBundle(),
+            new NjarebBundle\NjarebBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/NjarebBundle/NjarebBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace NjarebBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NjarebBundle extends Bundle
+{
+}
